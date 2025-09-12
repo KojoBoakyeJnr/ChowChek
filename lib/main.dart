87ad25b0524ba870130093f29f16c985e5c9bd80
KojoBoakye <kojoboakyejnr@outlook.com>
2025-09-12 01:09:53 +0000

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Aeonik"),
       routes: {
         AppRoutes.root: (context) => SplashScreen(),
